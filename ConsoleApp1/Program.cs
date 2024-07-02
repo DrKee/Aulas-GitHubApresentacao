e42289a0645b0b057d81a049fac7bfa565ee9f76
Willi <Willi@DESKTOP-9R23QNO>
2024-07-02 01:50:22 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+        }
+
+        public static int Soma(int numero1, int numero2)
+        {
+            return numero1 + numero2; 
+        }
+    } 
+}
